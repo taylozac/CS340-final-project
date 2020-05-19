caf6d45c477c150c293209e2ecd892631121a3db
Michael Hathaway <mikehath5@gmail.com>
2020-05-19 15:55:58 -0700

    styled homepage and recipes page, added recipe preview card, and updated routes.

--- routes/reciperRouter.js
+++ routes/reciperRouter.js
@@ -6,19 +6,26 @@ const router = express.Router();
 
 // recipe home page
 router.get("/", (req, res, next) => {
-  res.status(200).render("recipes");
+  res
+    .status(200)
+    .render("recipes", { css: ["recipes.css", "recipe_preview_card.css"] });
+});
+
+//get recipes for specific user
+router.get("/user/:u_id", (req, res, next) => {
+  let userID = req.params.u_id;
+  res
+    .status(200)
+    .render("recipes", {
+      userID: userID,
+      css: ["recipes.css", "recipe_preview_card.css"],
+    });
 });
 
 // recipe detail view
 router.get("/:r_id", (req, res, next) => {
   let recipeID = req.params.r_id;
   res.status(200).render("recipe_detail", { r_id: recipeID });
-});
-
-//get recipes for specific user
-router.get("/user/:u_id", (req, res, next) => {
-  let userID = req.params.u_id;
-  res.status(200).render("recipes", { userID: userID });
 });
 
 // export the router
