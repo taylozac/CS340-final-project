53267a3b32bd84d4ef606b351d65b7cb562d0095
Michael Hathaway <mikehath5@gmail.com>
2020-05-20 19:53:23 -0700

    add supplier page, add tool page, and add ingredient page

--- routes/supplierRouter.js
+++ routes/supplierRouter.js
@@ -8,4 +8,16 @@ router.get("/", (req, res, next) => {
   res.status(200).render("supplier", { css: ["supplier.css"] });
 });
 
+// add new ingredient page
+router.get("/add_ingredient", (req, res, next) => {
+  res
+    .status(200)
+    .render("add_ingredient_page", { css: ["add_ingredient.css"] });
+});
+
+// add new tool page
+router.get("/add_tool", (req, res, next) => {
+  res.status(200).render("add_tool_page", { css: ["add_tool.css"] });
+});
+
 module.exports = router;
