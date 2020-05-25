a154c7aa9815e14f3bc067820b384f2787566e44
Michael Hathaway <mikehath5@gmail.com>
2020-05-25 13:50:04 -0700

    Added basic form validation to login page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,12 +6,25 @@ const router = express.Router();
 
 //  base route leads to login page
 router.get("/", (req, res, next) => {
-  res.status(200).render("login", { css: ["login.css"] });
+  res
+    .status(200)
+    .render("login", { css: ["login.css"], js: ["login_form_validation.js"] });
 });
 
 // login page route
 router.get("/login", (req, res, next) => {
-  res.status(200).render("login", { css: ["login.css"] });
+  res
+    .status(200)
+    .render("login", { css: ["login.css"], js: ["login_form_validation.js"] });
+});
+
+// handle login post request
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+
+  // TODO: query database to check if user exist
+
+  res.status(400).send("success");
 });
 
 //register page route
---
+++ static/javascript/login_form_validation.js
@@ -0,0 +1,21 @@
+// get login form
+const loginForm = document.forms["login-form"];
+
+// add submit event listener
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); // prevent default form submission
+
+  // get username and password fields
+  let username = loginForm.elements["username"].value;
+  let password = loginForm.elements["password"].value;
+
+  // send username and password to the server
+  try {
+    let response = await axios.post("/login", {
+      username: username,
+      password: password,
+    });
+  } catch (err) {
+    alert("Username and password not recognized");
+  }
+});
