c52f01eff1643119cd79488d774efa9d6701a0c9
Eric Prather <prathere@oregonstate.edu>
2020-05-27 13:00:42 -0700

    Added support for new tools

--- routes/supplierRouter.js
+++ routes/supplierRouter.js
@@ -27,9 +27,32 @@ router.get(
 
 // add new tool page
 router.get("/add_tool", sessionMiddleware.ifNotLoggedin, (req, res, next) => {
-  res
-    .status(200)
-    .render("add_tool_page", { css: ["add_tool.css"], username: currentUSer });
+    let currentUser = req.session.username; // Needed?
+    res.status(200).render(
+        "add_tool_page",
+        { css: ["add_tool.css"], username: currentUser }
+    );
 });
+
+// Allow synthesis of new tool using existing form.
+// I was originally going to do this in the tool router but I changed my mind.
+router.post("/add_tool", sessionMiddleware.ifNotLoggedin, (req, res, next)=>{
+    try {
+        const { name, description } = req.body;
+
+        mysql.pool.query(
+            "INSERT INTO Tool (name, description) VALUES(?, ?)",
+            [name, description],
+            function (err, result) {
+                if(err)
+                    res.status(500).send("Error creating tool.");
+                else
+                    res.status(200).send("Created tool.");
+            }
+        ); // end query
+    } catch (err) {
+        res.status(500).send("Unexpected exception while creating tool.");
+    } // end catch
+}); // end router.post
 
 module.exports = router;
