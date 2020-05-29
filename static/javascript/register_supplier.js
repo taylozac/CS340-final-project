6c1a8637759856bdb90dfc5e58580edfad505f7e
Wilsondo <35271544+Wilsondo@users.noreply.github.com>
2020-05-29 12:28:20 -0700

    Supplier-Login-Test
    I have made some edits to files to allow for a supplier to register, hasn't been tested quite yet. To register as a supplier, you must go to the user registration page and navigate to the supplier registration.

---
+++ static/javascript/register_supplier.js
@@ -0,0 +1,39 @@
+// get login form
+const registerForm = document.forms["register-supplier-form"];
+
+// add submit event listener
+registerForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); // prevent default form submission
+
+  // get username and password fields
+  let username = registerForm.elements["username"].value;
+  let password = registerForm.elements["password"].value;
+  let passwordConfirm = registerForm.elements["confirm-password"].value;
+  console.log(username);
+  // check that passwords are the same
+  if (password !== passwordConfirm) {
+    alert("Passwords must match!");
+    return;
+  } else {
+    // send username and password to the server
+    try {
+        //The only difference in this script is that we post to a different link
+      let response = await axios.post("/register_supplier", {
+        username,
+        password,
+      });
+
+      if (response.data.wasSuccess) {
+        // user created
+        alert("Account created! Navigate to the login page and sign in!!");
+        registerForm.reset();
+      } else {
+        // username already in use
+        alert("username already in use!");
+        registerForm.reset();
+      }
+    } catch (err) {
+      alert("Error: An error has occurred");
+    }
+  }
+});
