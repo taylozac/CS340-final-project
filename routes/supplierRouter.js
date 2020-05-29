7a515cb7c9879095980ae0c87639273107b8c2be
Zachary-Taylor <taylozac@oregonstate.edu>
2020-05-29 09:45:11 -0700

    Display for ingredients/tools almost set up.

--- routes/supplierRouter.js
+++ routes/supplierRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("../dbcon.js");
 const sessionMiddleware = require("../sessionMiddleware.js");
+const async = require("async"); //Required to allow for the multiple SQL queries.
 
 const router = express.Router();
 
@@ -13,23 +14,78 @@ try {
     // Also, I'm not sure how to expand this to include ingredients. Is it possible
     // to double-up queries? Might no longer be appropriate to call the page
     // parameter "tools" anymore.
+
+    var return_data = {};
+
     mysql.pool.query(
-        "SELECT t.name, t.description FROM Tool t INNER JOIN manufactures m ON m.t_id = t.t_id INNER JOIN Supplier s ON s.s_id = m.s_id WHERE username=? ORDER BY t.t_id DESC;",
+        "SELECT t.name, t.description FROM Tool t INNER JOIN manufactures m ON m.t_id = t.t_id INNER JOIN Supplier sup ON sup.s_id = m.s_id WHERE username=? ORDER BY t.t_id DESC;",
         [currentUser], // This parameter is given to the SQL.
-        function(err, rows, fields) {
+        //function(err, tool_rows, fields) {
+        function(err, results) {
+            console.log(results);
             if(!err) { // No SQL Error
-                res.status(200).render("supplier", {
-                    css: ["supplier.css"],
-                    username: currentUser,
-                    tools: rows//, Next should be ingredients
-                }); // End render
+                //
+                // If there is not an error store our results in return_data.
+                //
+                return_data.currentUser = currentUser;
+                return_data.tools = results;
+
             } else {
                 res.status(500).send("Couldn't load the tools...\n" + err);
             }
         } // end SQL result handler
     ); // end SQL query block
+
+    mysql.pool.query(
+        "SELECT i.name, i.description FROM Ingredient i INNER JOIN stocks s ON s.i_id = i.i_id INNER JOIN Supplier sup ON sup.s_id = s.s_id WHERE username=? ORDER BY i.i_id DESC;",
+        [currentUser], // This parameter is given to the SQL.
+        function(err, results) {
+
+            //
+            // If there is not an error store our results in return_data.
+            //
+            if(!err) {
+                return_data.ingredients = results;
+            } else {
+                res.status(500).send("Couldn't load the tools...\n" + err);
+            }
+        } // end SQL result handler
+    ); // end SQL query block
+
+    console.log();
+    console.log();
+
+    res.status(200).render("supplier", {
+        css: ["supplier.css"],
+        username: return_data.currentUser,
+        ingredients: return_data.ingredients,
+        tools: return_data.tools//, Next should be ingredients
+    }); // End render*/
+
+
+
+/*
+    let found_tools = mysql.pool.query(
+        "SELECT t.name, t.description FROM Tool t INNER JOIN manufactures m ON m.t_id = t.t_id INNER JOIN Supplier s ON s.s_id = m.s_id WHERE username=? ORDER BY t.t_id DESC;",
+        [currentUser], // This parameter is given to the SQL.
+        function(err, rows, fields) {
+            if(err) {
+                res.status(500).send("Couldn't load the tools...\n" + err);
+            }
+        } // end SQL result handler
+    ); // end SQL query block
+
+    console.log(found_tools);
+
+    res.status(200).render("supplier", {
+        css: ["supplier.css"],
+        username: currentUser,
+        tools: found_tools.rows
+    });
+*/
+
 } catch(unknown) {
-    res.status(500).send("Unknown error detected while loading tools...");
+    res.status(500).send("Unknown error detected while loading ingredients and tools...");
 } // end try catch
 });
 
@@ -92,5 +148,31 @@ router.post("/add_tool", sessionMiddleware.ifNotLoggedin, (req, res, next)=>{
         res.status(500).send("Unexpected exception while creating tool.");
     } // end catch
 }); // end router.post
+
+//
+// This is virtually identical to the above code for adding a tool since they are
+// basically the same process. I will find something else to do as well.
+//
+router.post("/add_ingredient", sessionMiddleware.ifNotLoggedin, (req, res, next) => {
+    try {
+        const { name, description } = req.body;
+        mysql.pool.query(
+            "INSERT INTO Ingredient (name, description) VALUES(?, ?); INSERT INTO stocks (s_id, i_id) VALUES ((SELECT s_id FROM Supplier WHERE username = ?), (SELECT i_id FROM Ingredient WHERE name = ? AND description = ?))",
+            [name, description, req.session.username, name, description],
+            function (err, result) {
+                if(err)
+                {
+                    res.status(500).send("Error creating ingredient.\n" + err);
+                }
+                else
+                {
+                    res.status(200).redirect("/supplier");
+                }
+            }
+        );
+    } catch (err) {
+        res.status(500).send("Unexpected exception while creating ingredient.");
+    }
+});
 
 module.exports = router;
