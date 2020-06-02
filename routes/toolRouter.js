ee742ad8222b934530ede34a170d9ea0a50edba6
Eric Prather <prathere@oregonstate.edu>
2020-06-01 21:57:44 -0700

    Added tool detail page

--- routes/toolRouter.js
+++ routes/toolRouter.js
@@ -5,12 +5,13 @@ const express = require("express");
 const mysql = require("../dbcon.js"); // Not sure if this should be a hard-
                                       // -coded path...
 const sessionMiddleware = require("../sessionMiddleware.js");
+const async = require("async"); //???
 
 const router = express.Router();
 
 // No tools homepage, panic.
 router.get("/", (req, res, next)=>{
-    res.send("There is no tools home page");
+    res.status(500).send("There is no tools home page.");
 });
 
 // Show the details for a particular tool.
@@ -18,15 +19,42 @@ router.get("/:t_id", sessionMiddleware.ifNotLoggedin, (req, res, next)=>{
     let current_user = req.session.username;
     let tool_id = req.params.t_id;
     mysql.pool.query(
-        `SELECT * FROM Tool t WHERE t.t_id = ${tool_id}`,
+        "SELECT * FROM Tool t WHERE t.t_id = ?",
+        [req.params.t_id],
         function (err, rows, fields) { // error handler
             if(err) // If there was an error
+            {
                 res.status(500).send("Couldn't find that tool: " + tool_id);
-            res.status(200).render("tool_detail", {
-                css: [tool_detail.css],
-                tool: rows[0],
-                username: currentUser
-            }); // end res.status
+                return;
+            }
+
+            // Query 2: Get the suppliers of this tool.
+            mysql.pool.query(
+                "SELECT s.name, s.username FROM Supplier s INNER JOIN manufactures m ON m.s_id = s.s_id WHERE m.t_id = ?",
+                [req.params.t_id],
+                function (err2, results_2)
+                {
+                    var supplier_rows = [];
+                    var username_owns_tool = false;
+                    //console.log("Loading tool details...");
+                    //console.log(results_2);
+                    if(!err) {
+                        for(i = 0; i < results_2.length; i++){
+                            if(results_2[i].username == current_user){
+                                username_owns_tool = true;
+                            }
+                        }
+                        supplier_rows = results_2;
+                    }
+                    res.status(200).render("tool_detail", {
+                        css: ["tool_detail.css"],
+                        tool: rows[0],
+                        username: current_user,
+                        suppliers: supplier_rows,
+                        owns_tool: username_owns_tool
+                    }); // end res.status
+                }
+            );
         } // end response function
     ); // End mysql.pool.query
 }); // End router.get()
