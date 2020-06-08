a4bfdb02ea1f0761712c90c11e4cafecc4894302
Michael Hathaway <mikehath5@gmail.com>
2020-06-08 15:47:36 -0700

    added search bar functionality

--- static/javascript/recipe_search_bar.js
+++ static/javascript/recipe_search_bar.js
@@ -1,1 +1,20 @@
 let searchBar = document.forms["search-form"];
+let searchField = searchBar.elements["search-bar"];
+let allRecipes = document.querySelectorAll(".recipe-preview-card");
+
+searchBar.addEventListener("keyup", (event) => {
+
+  let searchValue = searchField.value.toLowerCase();
+  for (let recipe of allRecipes) {
+    // get title of the recipe
+    let recipeTitle = recipe.querySelector(".recipe-title").innerHTML.toLowerCase();
+
+    // if recipe title does not include search then hide it
+    if (!recipeTitle.includes(searchValue)) {
+      recipe.style.display = "none";
+    } else {
+      recipe.style.display = "";
+    }
+  }
+
+})
